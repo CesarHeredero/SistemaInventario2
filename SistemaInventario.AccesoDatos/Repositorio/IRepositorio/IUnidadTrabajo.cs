63e6da03cb106b48741384a7bf37ef44af00bbf0
CesarHeredero <heredero.cesar@gmail.com>
2021-01-21 12:40:45 +0100

    crud bodega

--- SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -7,5 +7,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
     public interface IUnidadTrabajo : IDisposable
     {
         IBodegaRepositorio Bodega { get; }
+
+        void Guardar();
     }
 }
