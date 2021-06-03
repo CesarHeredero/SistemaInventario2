497419c4502ba3472426d06e73b1934ba5d68636
CesarHeredero <heredero.cesar@gmail.com>
2021-06-03 16:37:47 +0200

    Añadir Sass al proyecto

---
+++ SistemaInventario/gulpfile.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('build-css', function () {
+    return gulp.src('./wwwroot/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('default', function () {
+    gulp.watch('./wwwroot/sass/**/*.scss', gulp.series('build-css'));
+});
